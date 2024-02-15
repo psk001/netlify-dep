244936bf757533a51f51ec6ba680d5732e2e1d9b
puspak <puspak9208@gmail.com>
2024-02-15 14:05:19 +0530

    t2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
+    <h3>sup ?? </h3>
     </div>
   );
 }
